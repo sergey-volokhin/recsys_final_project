c62f0aa0bf84c958310cb52bc27fca6fca4cba32
Sergey <>
2020-05-13 01:10:16 -0400

    added code

---
+++ process.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+
+all_features = ['text_tokens', 'hashtags', 'tweet_id', 'present_media', 'present_links', 'present_domains',\
+                'tweet_type', 'language', 'tweet_timestamp', 'engaged_with_user_id', 'engaged_with_user_follower_count',\
+                'engaged_with_user_following_count', 'engaged_with_user_is_verified', 'engaged_with_user_account_creation',\
+                'enaging_user_id', 'enaging_user_follower_count', 'enaging_user_following_count', 'enaging_user_is_verified',\
+                'enaging_user_account_creation', 'engagee_follows_engager']
+
+all_features_to_idx = dict(zip(all_features, range(len(all_features))))
+labels_to_idx = {'reply_timestamp': 20, 'retweet_timestamp': 21, 'retweet_with_comment_timestamp': 22, 'like_timestamp': 23}
+
+my_features = all_features[9:] + list(labels_to_idx.keys())
+rows = {i: [] for i in my_features}
+
+with open('new_train_short.tsv', encoding='utf-8') as f:
+
+    for line in f.readlines():
+        line = line.strip()
+
+        for name, value in zip(my_features, line.split('\x01')[9:]):
+            rows[name].append(value)
+
+
+df = pd.DataFrame(rows)
+df.to_csv('dataframe_again.tsv', sep='\t', index=False)
+
+
+def get_label(row):
+    if row['reply_timestamp']:
+        return 'reply_timestamp'
+    elif row['retweet_with_comment_timestamp']:
+        return 'retweet_with_comment_timestamp'
+    elif row['retweet_timestamp']:
+        return 'retweet_timestamp'
+    elif row['like_timestamp']:
+        return 'like_timestamp'
+    return 'no_engagement'
+
+
+df['label'] = df.apply(lambda row: get_label(row), axis=1)
+
+df = df.drop(['reply_timestamp', 'retweet_timestamp', 'retweet_with_comment_timestamp', 'like_timestamp'], axis=1)
+
+df.to_csv('class_dataframe.tsv', sep='\t', index=False)
---
+++ run_random_forest.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
+
+
+features = ['engaged_with_user_follower_count',
+            'engaged_with_user_following_count', 'engaged_with_user_is_verified',
+            'enaging_user_follower_count', 'enaging_user_following_count',
+            'enaging_user_is_verified',
+            'engagee_follows_engager']
+label_column = 'label'
+
+df = pd.read_csv('class_dataframe.tsv', sep='\t')
+df = df.loc[df['label'] != 'no_engagement']
+pd.concat([df.loc[df['label'] == 'like_timestamp'][:10000],
+           df.loc[df['label'] == 'reply_timestamp'][:10000],
+           df.loc[df['label'] == 'retweet_timestamp'][:10000],
+           df.loc[df['label'] == 'retweet_with_comment_timestamp'][:10000]], axis=1)
+
+X_train = df[features]
+y_train = df[label_column]
+
+print('done reading train')
+df = pd.read_csv('class_dataframe_val.tsv', sep='\t')
+df = df.loc[df['label'] != 'no_engagement']
+df = df[:100000]
+X_test = df[features]
+y_test = df[label_column]
+
+print('done reading test')
+
+clf = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=0)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+print('done training')
+results = f1_score(y_test, y_pred, average=None)
+
+print(results)
